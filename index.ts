0dc5ee189d4eb18f6ca74a8df8db09240e28331d
Łukasz Michalik <140089691+l-michalik@users.noreply.github.com>
2024-11-09 21:36:43 +0100

    work with routes

--- index.ts
+++ index.ts
@@ -3,23 +3,33 @@ import express, { Request, Response } from 'express';
 import { createLeagues } from './src/createLeagues';
 import { createTeams } from './src/createTeams';
 import { createStatistics } from './src/createStatistics';
+import cors from 'cors'; // Import cors
+import { League } from './src/models';
+import dbConnect from './src/lib/dbConnect';
 
 const app = express();
-
 const port: number = 4000;
 
 require("dotenv").config();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Root is running...');
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'OPTIONS'],
+}));
+
+app.get('/', async (req: Request, res: Response) => {
+    await dbConnect();
+    
+    const leagues = await League.find({});
+    
+    res.send(leagues);
 });
 
 app.listen(port, async () => {
-    // createLeagues();
-
-    // createTeams();
-
-    // createOrUpdateFixtures();
-
-    createStatistics();
+    // Uncomment these lines to run them when the server starts
+    // await createLeagues();
+    // await createTeams();
+    // await createOrUpdateFixtures();
+    // await createStatistics();
+    console.log(`Server is running on http://localhost:${port}`);
 });
